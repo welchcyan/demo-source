322d195b69e98eb7332157613f63149186f11b26
chenwbj <chenwbj@gmail.com>
2017-10-20 20:30:50 +0800

    add cpu comsumer

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,25 +2,38 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Random;
 
 
 @SpringBootApplication
-public class DemoApplication extends WebSecurityConfigurerAdapter{
+@RestController
+public class DemoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.authorizeRequests().and().csrf().ignoringAntMatchers("/admin/**");
-		http.antMatcher("/**")
-				.authorizeRequests()
-				.antMatchers("/index.html", "/admin/**", "/", "/api/catalog/**",
-						"/admin", "/assets/**").permitAll()
-				.anyRequest().authenticated().and().csrf().disable();
+
+	@RequestMapping( path = "/cpu" )
+	public void cpu(@RequestParam("level") String per) {
+
+		int loop = Integer.valueOf(per);
+		for( int j =0 ; j < loop; j++ ){
+			Thread t = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					for (int i = 0; i < 1000000000; i++) {
+						Random r = new Random();
+						r.setSeed(System.currentTimeMillis());
+						r.nextLong();
+					}
+				}
+			});
+			t.start();
+		}
+
 	}
 }
